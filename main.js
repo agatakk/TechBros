a8fe0d474afc613aab0853077ca78a273237b2d7
Agata Kubiak-Kokot <kubiak.aga@gmail.com>
2020-07-17 17:39:46 +0200

    hamburger lines transform (deg, translate)

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 const burger = document.querySelector('.menu-hamburger');
 const navList = document.querySelector('.main-menu');
 const menuItems = document.querySelectorAll('.main-menu__menu-item');
+const lines = document.querySelectorAll('.menu-hamburger div')
+
+const line1 = document.querySelector(".main-nav .menu-hamburger__line:first-child.toggle");
+
+
+
+console.log(lines);
 
 
 const navSlideIn = function(){
@@ -11,9 +18,13 @@ const navSlideIn = function(){
             link.style.animation = '';
         }else{
             link.style.animation = `navLinksFade .3s ease forwards ${index / 7 + 0.3}s`;
+
         }
     })
-    
+    lines.forEach(line=>{
+        line.classList.toggle('toggle');
+    })
 }
 
 burger.addEventListener('click', navSlideIn);
+console.log(line1);
